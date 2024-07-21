271506961bf34139fb8468e3fd1ced9d6aa3d46e
Mohammad Adhami <mohammad.adhami@muc.edu.ps>
2024-07-21 13:26:51 +0300

    change

--- config/database.js
+++ config/database.js
@@ -1,8 +1,9 @@
 // config/database.js
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('blog_9', 'root', '', {
-  host: 'localhost',
+const sequelize = new Sequelize('freedb_blog_9', 'freedb_adhami', '%3*4eq*@YkH2g42', {
+  host: 'sql.freedb.tech',
+  port:3306,
   dialect: 'mysql'
 });
 
